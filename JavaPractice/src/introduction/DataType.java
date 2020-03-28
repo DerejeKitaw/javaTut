ae9cd12df2e99f4fafbd2eb298123826d12db073
Dereje Kitaw <dereje.kitaw@gmail.com>
2020-03-28 16:10:08 -0400

    initial commit

---
+++ JavaPractice/src/introduction/DataType.java
@@ -0,0 +1,17 @@
+package introduction;
+
+import java.util.Date;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// Type ->lable(age)->value
+		int age = 10;
+		int myAge; 
+		// Short cut Sout +control + space
+		System.out.println(age);
+		// System.out.println(myAge);// Java variable must be initialized before "use"
+
+	}
+
+}
